31b78b8b7c68b5c6c53a50d5d45796764e6d3162
tqer39 <tqer39@gmail.com>
2024-07-10 23:05:18 +0900

    🔧 chore: Update Python version to 3.12.4 and add pyenv setup script

--- setup-repository.sh
+++ setup-repository.sh
@@ -7,3 +7,14 @@ if ! type brew > /dev/null 2>&1; then
 else
   echo "brew is already installed."
 fi
+
+# setup pyenv
+if ! type pyenv > /dev/null 2>&1; then
+  brew install pyenv
+  echo "pyenv installed."
+else
+  echo "pyenv is already installed."
+fi
+
+# install python
+pyenv install -s
