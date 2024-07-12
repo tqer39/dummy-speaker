5476c9f7c3f7720bf8cf5d853c13d7d4d66adf07
tqer39 <tqer39@gmail.com>
2024-07-13 00:33:40 +0900

    chore: Update setup-repository.sh with initial setup steps

--- setup-repository.sh
+++ setup-repository.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "========================================"
+echo "setup start."
+echo "========================================"
+
 # setup brew
 if ! type brew > /dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
